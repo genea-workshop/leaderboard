9cc9c46ff2cde3d84cca08ce63a923284f0acc52
nagyrajmund <nagyrajmund@users.noreply.github.com>
2025-10-28 07:10:12 +0000

    deploy: 7bfc5a3a98e517442102de9e26c984db398e258e

--- _app/immutable/chunks/CLsf1Tk_.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1ifebx7?.base??"/GENEAleaderboard",e=globalThis.__sveltekit_1ifebx7?.assets??s;export{e as a,s as b};
---
+++ _app/immutable/chunks/DCHVzSer.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_71wono?.base??"/GENEAleaderboard",a=globalThis.__sveltekit_71wono?.assets??s;export{a,s as b};
