836422f2bfbf7b48919c0fe84a42b753a5f25bc1
Rajmund Nagy <rajmund977@gmail.com>
2025-11-03 20:35:36 -0500

    Add test branch for new webpage

--- vite.config.ts
+++ vite.config.ts
@@ -8,5 +8,5 @@ export default defineConfig({
   // IMPORTANT: Update this to your repository name for GitHub Pages deployment.
   // For example, if your repository is at https://github.com/user/my-repo,
   // set base to '/my-repo/'.
-  base: '/super-duper-eureka.github.io/',
+  base: '/leaderboard/',
 })
